bfc472181cb19d5b6a44b79e922b3f5fca8ee786
zhiqinli@amazon.com <zhiqinli@amazon.com>
2022-10-09 11:07:22 +0800

    Upgrade V4L2Capturer to master
    Signed-off-by: zhiqinli@amazon.com <zhiqinli@amazon.com>

--- CMake/V4L2.cmake
+++ CMake/V4L2.cmake
@@ -4,7 +4,7 @@ if(BOARD STREQUAL "V4L2")
     include(ExternalProject)
     ExternalProject_Add(v4l2capturer
     GIT_REPOSITORY    https://github.com/codingspirit/V4l2Capturer.git
-    GIT_TAG           f9c2dcec06719f5bbc2e1cb9d9c16e8657deb97b
+    GIT_TAG           master
     CMAKE_ARGS        -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${BOARD_SDK_DIR}
     BUILD_ALWAYS      TRUE
     GIT_PROGRESS      TRUE
